30725e18607c04e4b87738334488f66ef829e36d
herstadd <herstadd@seattleu.edu>
2020-06-01 20:30:24 -0700

    Added documentation to Privacy.cshtml.cs
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/X9Vp68xSmky3FYbLxfzFnWQAAdbQ?Type=TaskLink&Channel=Link&CreatedTime=637266654115820000

--- LoydCrafts.WebSite/Pages/Privacy.cshtml.cs
+++ LoydCrafts.WebSite/Pages/Privacy.cshtml.cs
@@ -8,15 +8,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ReuseSU.WebSite.Pages
 {
+    /// <summary>
+    /// Code repository for the Privacy.cshtml.  Any code needed for this page should go here.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Aggregates most logging patterns to a single method
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor for the Privacy page to create a new privacy model
+        /// </summary>
+        /// <param name="logger"></param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Initializes the state needed for the page
+        /// </summary>
         public void OnGet()
         {
         }
